d9a24c89a980571888ab05ed0745bf1e68aea1ab
mouad mennioui <moad.menniui@gmail.com>
2024-07-04 17:22:34 +0100

    @notes card

---
+++ etunexus/app/Univercitise/Fields/Courses/chapter/@notes/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card";
+import React from "react";
+
+const Notes = () => {
+  return (
+    <div>
+      <Card>test started</Card>
+    </div>
+  );
+};
+
+export default Notes;
