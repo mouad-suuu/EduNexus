73435f03d06a5f4a9a07ab4afb4b71c3c4a1aeba
mouad mennioui <moad.menniui@gmail.com>
2024-07-06 21:53:15 +0100

    start from scratch

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-// global.d.ts
-import { Mongoose } from "mongoose";
-
-declare global {
-  var mongoose: {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-  };
-}
--- pdf-worker-loader.js
+++
@@ -1,2 +0,0 @@
-import { pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js`;
